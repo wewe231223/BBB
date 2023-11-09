6dfc4b3416dc4c9ddcd2aa443af270499063e4e5
wewe231223 <wewe231223@kpu.ac.kr>
2023-11-10 07:19:02 +0900

    robot 클래스 기본 작성

---
+++ BBB/Robot.cpp
@@ -0,0 +1,3 @@
+#include "pch.h"
+#include "Robot.h"
+#include "Input.h"
---
+++ BBB/Robot.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "Mesh.h"
+#include "Model.h"
+
+
+class Robot : public Object{
+
+public:
+	Robot();
+
+
+
+private:
+
+
+public:
+
+	virtual void update();
+	virtual void render();
+
+
+
+};
+
